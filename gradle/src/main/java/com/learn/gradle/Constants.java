71ee8bfce9f222e0a07dc4a32d30d430ad5d32a2
zhoushuoqi <zhoushuoqi@mininglamp.com>
2020-01-29 18:01:01 +0800

    gradle多模块应用

---
+++ gradle/src/main/java/com/learn/gradle/Constants.java
@@ -0,0 +1,5 @@
+package com.learn.gradle;
+
+public class Constants {
+    public static String name = "anni";
+}
---
+++ java/src/main/java/com/learn/java/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.learn.java;
+
+import com.learn.gradle.Constants;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println(Constants.name);
+    }
+}
